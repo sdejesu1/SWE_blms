aab555d69713ecc5e719c34b25475c41b0678bb6
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-02-05 11:43:08 -0500

    Added comments

--- Specs.py
+++ Specs.py
@@ -1,7 +1,12 @@
+# This function gets the users input and returns it
 def get_user_input():
     user_input = input(">> ")
     return user_input
 
+# This function splits the users input by spaces and adds it to a dictionary
+# such that the first input is the key and the second is the element and it
+# alternates in that pattern
+# this function returns the dictionary
 def parse_query(user_input):
     parsed_input = user_input.split(" ")
     print(parsed_input)
@@ -9,11 +14,13 @@ def parse_query(user_input):
     for x in range(0, len(parsed_input)-1, 2):
         dictionary_query.update({parsed_input[x]:parsed_input[x+1]})
         print(dictionary_query)
-    return "dictionary"
+    return dictionary_query
 
+# This function will be used to execute the query made in the parser
 def execute_query():
     return "results of the query execution"
 
+# the following code runs the code above
 looper = True
 while looper == True:
     user_input = get_user_input()
