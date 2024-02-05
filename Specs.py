ea7536391afd3bc13d8842d9322fdd715ec31359
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-02-05 11:24:50 -0500

    updated some functions in specs.py and added some functionality to run the user input

--- Specs.py
+++ Specs.py
@@ -1,5 +1,18 @@
-def parse_query():
+def get_user_input():
+    user_input = input(">> ")
+    return user_input
+
+def parse_query(user_input):
     return "dictionary"
 
 def execute_query():
     return "results of the query execution"
+
+looper = True
+while looper == True:
+    user_input = get_user_input()
+    if user_input.lower() == "help":
+        print('help notes')
+    if user_input.lower() == "quit":
+        print('Quitting...')
+        looper = False
