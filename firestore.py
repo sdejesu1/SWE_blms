087592f123d2ff6479db5d719c1cbc3399865551
Mish Wilson <mwilso33@uvm.edu>
2024-02-05 11:10:56 -0500

    Fixed the charmap encoding problem

--- firestore.py
+++ firestore.py
@@ -1,20 +1,19 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 import json
 
-#Set up Firestore
+# Set up Firestore
 cred = credentials.Certificate('soft-eng-warmup-a838c198caa2.json')
-
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-#Open JSON File
-f = open('Music_Artists.json')
-data = json.load(f)
+# Open JSON File with explicit encoding
+with open('Music_Artists.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
-# add_data
+# Add data to Firestore
 for artist in data['spotify_artists']:
-    # doc_ref = db.collection("test-music").document()
-    # doc_ref.set(artist)
+    # Assuming 'test-music' is your collection name
+    doc_ref = db.collection("test-music").document()
+    doc_ref.set(artist)
     print(artist)
