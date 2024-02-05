dfb58887f7c0cb740cf5703fd6076a3a08d56f16
Steven De Jesus <sdejesu1>
2024-02-05 11:31:38 -0500

    added smaple query

--- firebase_query.py
+++ firebase_query.py
@@ -2,7 +2,15 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 
+from google.cloud.firestore_v1 import FieldFilter
+
 # Set up Firestore
 cred = credentials.Certificate('soft-eng-warmup-a838c198caa2.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+music_ref = db.collection("test-music")
+
+query = music_ref.where(filter=FieldFilter("Artist Name", "==", "Bad Bunny")).stream()
+for queue in query:
+    print(queue)
